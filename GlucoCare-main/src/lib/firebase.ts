ce351999eedfcd6fc4485f9f42f6679e3eb01d5c
khusbooagarwal886 <148992224+khusbooagarwal886@users.noreply.github.com>
2026-07-26 16:03:36 +0530

    Update firebase.ts

--- GlucoCare-main/src/lib/firebase.ts
+++ GlucoCare-main/src/lib/firebase.ts
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { initializeFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
@@ -16,5 +16,7 @@ export const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
-export const db = getFirestore(app);
+export const db = initializeFirestore(app, {
+  ignoreUndefinedProperties: true,
+});
 export const storage = getStorage(app);
